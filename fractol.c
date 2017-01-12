690830b9c7efa3a17b39cb8d558b2e243a0d8653
Can Yildirim <cyildiri@student.42.us.org>
2017-01-12 02:04:21 -0800

    added support for mouse dragging

--- fractol.c
+++ fractol.c
@@ -22,14 +22,14 @@ typedef struct	s_fractol
 }				t_fractol;
 
 
+
+
 int get_iterations(float x, float y, int max_iterations, t_fractol *fractol)
 {
 	int cur_iter;
 	float x0;
 	float y0;
 	float xtemp;
-
-
 
 	x0 = 1.5 * (x - fractol->renderer->win_x / 2) / (0.5 * fractol->zoom * fractol->renderer->win_x) + fractol->pos.x;
 	y0 = (y - fractol->renderer->win_y / 2) / (0.5 * fractol->zoom * fractol->renderer->win_y) + fractol->pos.y;
@@ -96,6 +96,52 @@ void render2d(t_renderer *renderer, t_fractol *fractol)
 	mlx_destroy_image(renderer->mlx, renderer->scene->cur_frame.id);
 }
 
+int		mouse_press_hook(int button, int x, int y, void *param)
+{
+	t_fractol	*fractol;
+
+	fractol = (t_fractol *)param;
+	x -= 500;
+	y -= 500;
+	if (button && x && y)
+		ft_putchar('\0');
+	fractol->renderer->last_click.x = x;
+	fractol->renderer->last_click.y = y;
+	return (0);
+}
+
+int		mouse_release_hook(int button, int x, int y, void *param)
+{
+	t_fractol	*fractol;
+
+	fractol = (t_fractol *)param;
+	x -= 500;
+	y -= 500;
+	if (button)
+		ft_putchar('\0');
+	fractol->renderer->last_click.x = -99;
+	fractol->renderer->last_click.y = -99;
+	return (0);
+}
+
+int		mouse_motion_hook(int x, int y, void *param)
+{
+	t_fractol	*fractol;
+
+	fractol = (t_fractol *)param;
+	x -= 500;
+	y -= 500;
+	if (fractol->renderer->last_click.x != -99 && fractol->renderer->last_click.y != -99)
+	{
+		fractol->pos.x -= (x - fractol->renderer->last_click.x) / (fractol->renderer->win_x / 3 * fractol->zoom);
+		fractol->pos.y -= (y - fractol->renderer->last_click.y) / (fractol->renderer->win_y / 2 * fractol->zoom);
+		fractol->renderer->last_click.x = x;
+		fractol->renderer->last_click.y = y;
+		render2d(fractol->renderer, fractol);
+	}
+	return (0);
+}
+
 int		key_pressed(int keycode, void *param)
 {
 	t_fractol	*fractol;
@@ -142,16 +188,25 @@ int main()
 	t_fractol fractol;
 	t_scene		*scene1;
 
+	int winx;
+	int winy;
+
+	winx = 500;
+	winy = 500;
 	fractol.renderer = new_renderer(render_scene);
 	fractol.pos.x = -0.5;
 	fractol.pos.y = 0;
 	fractol.zoom = 1.0;
 	fractol.iterations = 64;
-	add_window(fractol.renderer, 1000, 1000, "cyildiri's fract'ol");
-	scene1 = new_scene(perspective_projection, 1000, 1000);
+	add_window(fractol.renderer, winx, winy, "cyildiri's fract'ol");
+	scene1 = new_scene(perspective_projection, winx, winy);
 	fractol.renderer->scene = scene1;
 	render2d(fractol.renderer, &fractol);
 	mlx_hook(fractol.renderer->window, 2, 0, key_pressed, &fractol);
+
+	mlx_hook(fractol.renderer->window, 4, 0, mouse_press_hook, &fractol);
+	mlx_hook(fractol.renderer->window, 5, 0, mouse_release_hook, &fractol);
+	mlx_hook(fractol.renderer->window, 6, 0, mouse_motion_hook, &fractol);
 	mlx_loop(fractol.renderer->mlx);
 	return (0);
 }
